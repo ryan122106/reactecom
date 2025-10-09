c73e59441fb3f655ddbaf1d7d65d84081abc1071
Ryan <ryan.sensei21@gmail.com>
2025-10-09 14:41:11 +0800

    c

--- src/utils/api_user.js
+++ src/utils/api_user.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+  import axios from "axios";
 
-import { API_URL } from "./constants";
+  import { API_URL } from "./constants";
 
-export const login = async (email, password) => {
-  const response = await axios.post(API_URL + "users/login", {
-    email,
-    password,
-  });
-  return response.data;
-};
+  export const login = async (email, password) => {
+    const response = await axios.post(API_URL + "users/login", {
+      email,
+      password,
+    });
+    return response.data;
+  };
 
-export const signup = async (name, email, password) => {
-  const response = await axios.post(API_URL + "users/signup", {
-    name,
-    email,
-    password,
-  });
-  return response.data;
-};
+  export const signup = async (name, email, password) => {
+    const response = await axios.post(API_URL + "users/signup", {
+      name,
+      email,
+      password,
+    });
+    return response.data;
+  };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = "http://localhost:5123/";
+
+export const API_URL = import.meta.env.DEV ? 
+"http://localhost:5123/api/" : "http://b15-piriyan.mak3r.dev/api/";
